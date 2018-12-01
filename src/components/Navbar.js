1af9c5aa03e69e1bcde0b9e81ef52ef7a70442bd
Marunizer <mendez1226@gmail.com>
2018-11-30 23:51:13 -0500

    Working test (: Limiting to just windows cause Linux takes a year

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,7 +10,7 @@ const Navbar = () => {
     // }, 1500)
 
     return (//JSX
-        <nav className="nav-wrapper blue darken-4">
+        <nav className="nav-wrapper green darken-4">
             <div className="container">
                 <Link to="/" className="brand-logo right">Smasherz</Link>
                 <ul className="left">
